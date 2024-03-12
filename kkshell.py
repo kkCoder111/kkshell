ffdd6f37f317d1b5448057c2e710bbd6024392d2
kkCoder111 <90219573+kkCoder111@users.noreply.github.com>
2024-03-12 13:49:34 -0700

    Adding some commands

--- kkshell.py
+++ kkshell.py
@@ -39,31 +39,59 @@ def cd(dir):
     global winualias
     global cwd
     global nix_path
+    global path_snapshot
+    try:
+        if dir != " " and dir != "..":
+            print (dir)
+            if dir in winualias.keys():
+                cwd = winualias[dir]
+            elif dir[0] == "/" and ostype == "win":
+                olddrive = dir[0:3]
+                newdrive = dir[1] + ":\\"
+                dir = dir.replace(olddrive, newdrive)
+                dir = dir.replace("/", "\\")
+                cwd = dir
+            else:
+                cwd = dir
+            cwd_files = getfiles(cwd)
+            cwd_dirs = getdirs(cwd)
 
-    if dir != " ":
-        if dir in winualias.keys():
-            cwd = winualias[dir]
-        elif dir[0] == "/" and ostype == "win":
-            olddrive = dir[0:3]
-            newdrive = dir[1] + ":\\"
-            dir = dir.replace(olddrive, newdrive)
-            cwd = dir
+            nix_path = cwd
+
+            if ":\\" in cwd:
+                drive = cwd[0]
+                olddrive = drive + ":\\"
+                newdrive = "/" + drive + "/"
+                nix_path = nix_path.replace(olddrive, newdrive)
+                nix_path = nix_path.replace("//", "/")
+            if "\\" in cwd:
+                nix_path = nix_path.replace("\\", "/")
+            path_snapshot = cwd
+        elif dir == "..":
+            path_split = nix_path.split("/")
+            path_split.remove(path_split[len(path_split)-1])
+            path_split = +"/" + "/".join(path_split)
+            print(path_split)
+        elif dir == "-d":
+            pwd(dir)
         else:
-            cwd = dir
-        cwd_files = getfiles(cwd)
-        cwd_dirs = getdirs(cwd)
-
-        nix_path = cwd
-
-        if ":\\" in cwd:
-            drive = cwd[0]
-            olddrive = drive + ":\\"
-            newdrive = "/" + drive + "/"
-            nix_path = nix_path.replace(olddrive, newdrive)
-        if "\\" in cwd:
-            nix_path = nix_path.replace("\\", "/")
-    else:
-        print("cd - no directory was specified")
+            print("error - no directory was specified")
+    except FileNotFoundError:
+        try:
+            if ostype == "win":
+                dir = dir.replace("/", "")
+                dir = dir.replace("\\", "")
+                todir = path_snapshot + "\\" + dir
+            elif ostype == "nix":
+                dir = dir.replace("/", "")
+                dir = dir.replace("\\", "")
+                todir = path_snapshot + "/" + dir
+            print(todir)
+            cd(todir)
+        except OSError:
+            print("cd - cannot find path '" + dir + "'")
+    finally:
+        nix_path = nix_path.replace("//", "/")
 
 def ld(arg):
     if arg == " ":
@@ -83,6 +111,38 @@ def ld(arg):
         print(" ")
     else:
         print("Invalid option:", arg)
+def start(arg):
+    if ostype == "nix":
+        print("s is not currently available on Linux")
+    else:
+        tostart = "start " + arg
+        os.system(tostart)
+
+def install(arg):
+    if ostype == "nix":
+        print("install is only available on Windows")
+    else:
+        todo = "winget install " + arg
+        os.system(todo)
+def uninstall(arg):
+    if ostype == "nix":
+        print("uninstall is only available on Windows")
+    else:
+        todo = "winget uninstall " + arg
+
+def dci(arg):
+    if arg != " " or arg != "":
+        os.system(arg)
+    else:
+        print("dci: no command was specified")
+def pwd(dummy):
+    print("True working directory:", cwd)
+    print("Unix-like working directory (Windows only):", nix_path)
+    if dummy != " " or dummy != "":
+        print("Note: this command takes no attributes")
+
+def d(path):
+    cd(path)
 
 def interpret(command):
     cmd = command[0].lower()
